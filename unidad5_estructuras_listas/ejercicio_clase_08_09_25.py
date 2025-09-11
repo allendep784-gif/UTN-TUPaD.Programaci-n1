6d65cf93727ef1d6bd3dd41650e6d417b05e2067
Pablo Allende <allendep784@gmail.com>
2025-09-11 00:05:29 -0300

    subo correctamente corrección

--- unidad5_estructuras_listas/ejercicio_clase_08_09_25.py
+++ unidad5_estructuras_listas/ejercicio_clase_08_09_25.py
@@ -1,64 +1,72 @@
 # Ejercicio Bingo
+# ENTRADA
+
 import random
+# Definimos los números que pueden ser sorteados y mesclamos su orden de forma aleatorea
+numeros_sorteados = list(range(1, 51))
+random.shuffle(numeros_sorteados)
+
+# Definimos dos variables para acceder al valor de fila y columna del carton
 cantidad_filas = 5
 cantidad_columnas = 5
+
+# Definimos una matriz con 25 elementos no repetidos en un rango de 1 y 50 
 numeros_carton = random.sample(range(1, 51), 25)
 carton = [numeros_carton[i:i+5] for i in range(0, 25, 5)]
-numero_sorteado = 0
-carton_en_cero = True
-no_gana = True
+
+# Variable que permite pasar al siguiente número a buscar
+numero_encontrado = False
+
+gano = False
+
+# PROCESO / SALIDA
 
 # Se le muestra el carton al usuario
-print("Tu carton es:")
+print("Tu cartón es: \n")
 for fila in carton:
     for columna in fila:
         print(columna, end = " ")
     print()
-    
+print()
 
-# En caso de que la condición no gana sea cierta se ejecuta el bucle
-while no_gana:
-    # Genera número aleatoreo entre 1 y 50
-    numero_sorteado = random.randint(1, 50)
-    # Muestra el nro por pantalla
-    print(f"Número sorteado {numero_sorteado} \n")
-
-    for posicion_fila in range(cantidad_filas):
+# Bucle que recorre las 50 posiciones de la lista numeros_sorteados tomando el elemento de cada posicion
+for numero in numeros_sorteados:
+    print(f"Numero sorteado : {numero}")
+    numero_encontrado = False
+    # Bucle anidado que se fija si, el nro tomado en el ciclo anterior esta o no en la posicion fila y columna del cartón 
+    for posicion_filas in range(cantidad_filas):
         for posicion_columna in range(cantidad_columnas):
-            # Se fija si la posicion coincide con el valor
-            if carton[posicion_fila][posicion_columna] == numero_sorteado:
-                print("¡Lo tenés! \n")
-                # Si es cierto se cambia el valor de dicha posición por 0
-                carton[posicion_fila][posicion_columna] = 0
-                # Se muestra el carton actualizado
+            # En caso de que el nro tomado se encuentre en la posición se remplazara por un cero y le dira al usuario, ¡Lo Tenés!
+            # Si no lo encuentra lo seguira buscando en las otras posiciones y en caso de que no lo tenga se lo comunicara al usuario
+            # Además se muestra el cartón actualizado al usuario
+            if carton[posicion_filas][posicion_columna] == numero:
+                print("¡Lo Tenés! \n")
+                carton[posicion_filas][posicion_columna] = 0
                 for fila in carton:
                     for columna in fila:
                         print(columna, end = " ")
                     print()
-
-# Verificacion para saber si gano o no
-    for posicion_fila in range(cantidad_filas):
-        for posicion_columna in range(cantidad_columnas):
-            # Si se encuentra en alguna posición un elemento distinto de 0, carton_en_cero va a ser igual a falso
-            # para que se siga ejecutando el bucle while, hasta que el carton sea igaul a cero
-            if carton[posicion_fila][posicion_columna] != 0:
-                carton_en_cero = False
+                print()
+                numero_encontrado = True
+                # Sale del bucle
                 break
-            else:
-                carton_en_cero = True
-
-        # En caso de que el carton no este en 0
-        if carton_en_cero == False:
-            # Sale del bucle para seguir jugando
+        # Si el nro se encontro en alguna columna de la fila, sale del bucle, para buscar un nuevo nro
+        if numero_encontrado == True:
             break
-        else:
-            # Si no, no_gana va a ser falso para salir del bucle
-            no_gana = False
 
-print()
-# Una ves sale del bucle se muestra el mensaje ¡Bingo! segido del carton con toas sus posiciones en cero para que el usuario sepa que gano
-print("¡Bingo!")
-for fila in carton:
-    for columna in fila:
-        print(columna, end = " ")
-    print()
+    if numero_encontrado == False:
+        print("No lo tenés \n")
+
+# Bucle para saber si gano
+    for posicion_filas in range(cantidad_filas):
+        for posicion_columna in range(cantidad_columnas):
+            if carton[posicion_filas][posicion_columna] == 0:
+                gano == True
+            else:
+                gano == False
+                break
+        if gano == False:
+            break
+
+if gano == True:
+    print("¡Bingo!")
